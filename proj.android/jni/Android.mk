151b323ce0438a4522fe275847e565ef4dedbee2
shamaton <shamaton@hoge.com>
2015-10-03 17:53:29 +0900

    アンドロイドでビルドが通るように

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -10,31 +10,46 @@ LOCAL_MODULE := cocos2dcpp_shared
 
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
-LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+# cpp and c files
+LOCAL_SRC_FILES_JNI_PREFIXED := \
+    $(wildcard $(LOCAL_PATH)/../../Classes/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/../../Classes/**/*.c*) \
+    $(wildcard $(LOCAL_PATH)/../../Classes/**/**/*.c*) \
+    $(wildcard $(LOCAL_PATH)/nend/*.cpp)
 
+LOCAL_SRC_FILES_JNI_UNPREFIXED := $(subst jni/,, $(LOCAL_SRC_FILES_JNI_PREFIXED))
+
+LOCAL_SRC_FILES := hellocpp/main.cpp \
+                   $(LOCAL_SRC_FILES_JNI_UNPREFIXED)
+
+
+# ヘッダのincludeパスを追加
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../cocos2d/external
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/nend
+# LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../Classes/Android
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
+LOCAL_WHOLE_STATIC_LIBRARIES += spine_static
 
 # LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
 # LOCAL_WHOLE_STATIC_LIBRARIES += cocosbuilder_static
 # LOCAL_WHOLE_STATIC_LIBRARIES += spine_static
 # LOCAL_WHOLE_STATIC_LIBRARIES += cocostudio_static
 # LOCAL_WHOLE_STATIC_LIBRARIES += cocos_network_static
-# LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
 
 
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,.)
 $(call import-module,audio/android)
+$(call import-module,extensions)
+$(call import-module,editor-support/spine)
 
 # $(call import-module,Box2D)
 # $(call import-module,editor-support/cocosbuilder)
 # $(call import-module,editor-support/spine)
 # $(call import-module,editor-support/cocostudio)
 # $(call import-module,network)
-# $(call import-module,extensions)
