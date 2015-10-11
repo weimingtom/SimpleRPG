987811b5989c47acef41ace746ae00cee2f20eb4
shamaton <shamaton@hoge.com>
2015-10-12 00:19:12 +0900

    delegateを更新

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -6,6 +6,11 @@
 
 USING_NS_CC;
 
+static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
+static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
+static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
+static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
+
 AppDelegate::AppDelegate() {
 
 }
@@ -14,14 +19,63 @@ AppDelegate::~AppDelegate()
 {
 }
 
+//if you want a different context,just modify the value of glContextAttrs
+//it will takes effect on all platforms
+void AppDelegate::initGLContextAttrs()
+{
+    //set OpenGL context attributions,now can only set six attributions:
+    //red,green,blue,alpha,depth,stencil
+    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
+
+    GLView::setGLContextAttrs(glContextAttrs);
+}
+
+// If you want to use packages manager to install more packages, 
+// don't modify or remove this function
+static int register_all_packages()
+{
+    return 0; //flag for packages manager
+}
+
 bool AppDelegate::applicationDidFinishLaunching() {
     // initialize director
     auto director = Director::getInstance();
     auto glview = director->getOpenGLView();
     if(!glview) {
-        glview = GLView::create("My Game");
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
+        glview = GLViewImpl::createWithRect("SimpleRPG", Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+#else
+        glview = GLViewImpl::create("SimpleRPG");
+#endif
         director->setOpenGLView(glview);
     }
+
+    // turn on display FPS
+    director->setDisplayStats(true);
+
+    // set FPS. the default value is 1.0/60 if you don't call this
+    director->setAnimationInterval(1.0 / 60);
+
+    /*
+    // Set the design resolution
+    glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::NO_BORDER);
+    Size frameSize = glview->getFrameSize();
+    // if the frame's height is larger than the height of medium size.
+    if (frameSize.height > mediumResolutionSize.height)
+    {        
+        director->setContentScaleFactor(MIN(largeResolutionSize.height/designResolutionSize.height, largeResolutionSize.width/designResolutionSize.width));
+    }
+    // if the frame's height is larger than the height of small size.
+    else if (frameSize.height > smallResolutionSize.height)
+    {        
+        director->setContentScaleFactor(MIN(mediumResolutionSize.height/designResolutionSize.height, mediumResolutionSize.width/designResolutionSize.width));
+    }
+    // if the frame's height is smaller than the height of medium size.
+    else
+    {        
+        director->setContentScaleFactor(MIN(smallResolutionSize.height/designResolutionSize.height, smallResolutionSize.width/designResolutionSize.width));
+    }
+     */
     
     // チラツキ対応
     director->getInstance()->setProjection(Director::Projection::_2D);
@@ -29,15 +83,8 @@ bool AppDelegate::applicationDidFinishLaunching() {
     
     
     glview->setDesignResolutionSize(GAME_RESOLUTION.width, GAME_RESOLUTION.height, ResolutionPolicy::SHOW_ALL);
-    
-    //glview->setDesignResolutionSize(800, 320, ResolutionPolicy::SHOW_ALL);
-    //director->setContentScaleFactor(2.0f);
 
-    // turn on display FPS
-    director->setDisplayStats(true);
-
-    // set FPS. the default value is 1.0/60 if you don't call this
-    director->setAnimationInterval(1.0 / 60);
+    register_all_packages();
     
     // とりあえずの初期化
     auto gm = GameManager::getInstance();
@@ -45,8 +92,9 @@ bool AppDelegate::applicationDidFinishLaunching() {
     gm->set_map_init_position(9, 9);
 
     // create a scene. it's an autorelease object
-    //auto scene = MapScene::createScene();
-    auto scene = Game::createScene();
+    auto scene = MapScene::createScene();
+    //auto scene = Game::createScene();
+    
     // run
     director->runWithScene(scene);
 
